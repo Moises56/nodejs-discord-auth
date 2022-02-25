dfaaccbf98914f558770a11a8b2e097ed1732be1
moises56 <moiavil56@gmail.com>
2022-02-24 22:47:33 -0500

    server port

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const app = require('./app');
 const {PORT} = require('./config');
 require('./db');
 
-app.listen( app.get('port'))
-console.log('Server on Port',  app.get('port'));
+// app.listen( app.get('port'))
+// console.log('Server on Port',  app.get('port'));
 
 
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
